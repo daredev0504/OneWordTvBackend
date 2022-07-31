a75053bb377a0e60def70f0f053d7acb7b7adad7
Tosingh54 <peter.adebimpe@kuda.com>
2022-07-31 17:51:15 +0100

    basic setup

---
+++ OneWordTvBackend/WeatherForecast.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace OneWordTvBackend
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int Temperature { get; set; }
+        public bool IsTraining { get; set; }
+        public string Summary { get; set; }
+        public int Age { get; set; }
+
+        public string Heigth { get; set; }
+        public char Character { get; set; }
+    }
+}
